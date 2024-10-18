a590fcf2dd81f12b7ee2ed1790517c174cb1a7f2
worshix <153189473+worshix@users.noreply.github.com>
2024-10-18 13:18:17 +0200

    margin top auto

--- src/components/latestPosts/Post.jsx
+++ src/components/latestPosts/Post.jsx
@@ -35,7 +35,7 @@ const Post = ({image, title, content, author, date, link, type}) => {
         <p className="font-light text-slate-600">{summary.join(" ") + "..."}</p>
         <Link type="button" className="flex text-black gap-[2px] services-link my-2 ml-auto h-fit p-2 text-xs w-fit bg-slate-300 rounded-lg hover:shadow-md shadow-black active:scale-95" href={link}>read more</Link>
     </div>
-      <div className="post-card-details bg-main-500 p-2 text-white">
+      <div className="post-card-details bg-main-500 p-2 text-white mt-auto">
         <span className="block">by {author}</span>
 	      <time className="text-xs"
   	      datetime={
