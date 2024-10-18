85b9f055b4073565e980f404bad7623bd7276431
worshix <153189473+worshix@users.noreply.github.com>
2024-10-18 13:32:23 +0200

    links on /media/page.jsx back

--- src/app/media/page.jsx
+++ src/app/media/page.jsx
@@ -31,11 +31,11 @@ return (
   <>
   	<h1 className="text-4xl font-bold my-4 text-center text-main-400 w-full flex-shrink-0">Media</h1>
       {/* links to pages for events, news, articles */}
-{/*       <section className='flex gap-2 justify-center'>
+      <section className='flex gap-2 justify-center'>
           <Link className='btn-main' href="/media/events">Events</Link>
           <Link className='btn-main' href="media/articles">Article</Link>
           <Link className='btn-main' href="media/news">News</Link>
-      </section> */}
+      </section>
       <h1 className='section-heading slide-in-top'>Gallery</h1>      
       {/* the gallery Slide here down here */}
       <Gallery images={images} />
